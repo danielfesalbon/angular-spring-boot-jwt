b2e09bdabbac1c9028ae1b52ede1677757f53068
Daniel Fesalbon <35003912+dnQuinn@users.noreply.github.com>
2020-02-04 22:14:42 +0800

    Add files via upload

--- CMS/src/app/pages/home/home.component.ts
+++ CMS/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientService } from 'src/app/services/http-client.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpClientService) { }
 
   ngOnInit() {
+    setInterval(() => {
+      //code goes here that will be run every 1 hr.  
+      this.checkToken();
+    }, 3600000);
+  }
+
+  checkToken() {
+    //let time = (new Date(sessionStorage.getItem('expiration')).getTime() - new Date().getTime());
+    let t = new Date(sessionStorage.getItem('expiration')).valueOf() - new Date().valueOf();
+    let time = t / 3600000;
+    console.log(new Date(time).getMinutes());
+    console.log(sessionStorage.getItem('jwt'));
+    if (time < 1) {
+      console.log('token expiring in 1hr..')
+      //if expiring within 1 hr
+      if (sessionStorage.getItem('jwt') != null && sessionStorage.getItem('jwt') != undefined) {
+        console.log("refreshing token");
+        this.httpService.refreshToken().subscribe(res => {
+          console.log(res);
+          if (res.hasOwnProperty('jwt')) {
+            sessionStorage.setItem('jwt', res['jwt']);
+            sessionStorage.setItem('expiration', res['expiration'].toString());
+          }
+        });
+      }
+    }
   }
 
 }
